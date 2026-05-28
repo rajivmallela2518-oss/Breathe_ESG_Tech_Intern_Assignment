0077a678578b53daa19625ea620be6ddd9e70428
Rajiv Mallela <rajivmallela2518@gmail.com>
2026-05-28 21:20:38 +0530

    Add TEMPLATES setting to base config

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -46,6 +46,22 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
 ROOT_URLCONF = "config.urls"
 AUTH_USER_MODEL = "users.TenantUser"
 
